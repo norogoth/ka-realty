1c90ca91f8068a60300e076be138022e970bd634
norogoth <norogoth@gmail.com>
2021-07-23 14:30:53 -0700

    trying to deploy to Netlify again

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-styled-components',
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
